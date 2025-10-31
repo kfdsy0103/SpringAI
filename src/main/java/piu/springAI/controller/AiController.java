9683280bdcf1d2450f0fa8f3d1e9e4147ac45b20
pywoo <kfdsy0103@naver.com>
2025-10-31 17:31:21 +0900

    feat: ChatModel만 사용하여 기능 구현

---
+++ src/main/java/piu/springAI/controller/AiController.java
@@ -0,0 +1,41 @@
+package piu.springAI.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import piu.springAI.service.AiService;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/ai")
+public class AiController {
+
+	private final AiService aiService;
+
+	@PostMapping(
+		value = "/chat-model",
+		consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+		produces = MediaType.TEXT_PLAIN_VALUE
+	)
+	public String chatModel(@RequestParam("question") String question) {
+		String answerText = aiService.generateText(question);
+		return answerText;
+	}
+
+	@PostMapping(
+		value = "/chat-model-stream",
+		consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+		produces = MediaType.APPLICATION_NDJSON_VALUE
+	)
+	public Flux<String> chatModelStream(@RequestParam("question") String question) {
+		Flux<String> answer = aiService.generateStreamText(question);
+		return answer;
+	}
+}
